01bebb566a3a65fcd9a08829fe425078b09bbb05
Suhas Bharadwaj <suhas.five@gmail.com>
2018-12-12 15:42:26 +0530

    Added a dummy login page

---
+++ src/Feature/Login/code/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using SitecoreLogin.Feature.Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreLogin.Feature.Login.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index(LoginViewModel loginViewModel = null)
+        {
+            return View(loginViewModel ?? new LoginViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult LoginUser(LoginViewModel loginViewModel)
+        {
+            loginViewModel.IsLoginSuccess = true;
+            return View("~/Views/Login/Index.cshtml", loginViewModel);
+        }
+    }
+}
---
+++ src/Feature/Login/code/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreLogin.Feature.Login.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsLoginSuccess { get; set; }
+    }
+}
